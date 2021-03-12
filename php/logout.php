95dc1743857b1c974cb33b473af7822b984481ec
RUI YANG <rui.yang@city.ac.uk>
2021-03-12 15:59:00 +0000

    add setCookie
    too maintain exit status

--- php/logout.php
+++ php/logout.php
@@ -2,8 +2,7 @@
     session_start();
     unset($_SESSION['email_login']); // disable the username session
     unset($_SESSION['role']); // disable the username session
-
+    setCookie(session_name(),'',time()-3600,'/');
     session_destroy();
-
     header("Location: ../index.php");
 ?>
