585886997533ac5a72e92678c79d6d59e29e6bc5
RUI YANG <rui.yang@city.ac.uk>
2021-03-12 16:21:38 +0000

    Pages support Doc
    needed for pages done by bootstrap

---
+++ included/footer.php
@@ -0,0 +1,16 @@
+            <footer class="text-primary bg-white sticky-footer" style="background: repeating-radial-gradient(black 0%, rgb(36,36,38) 0%), var(--blue);color: var(--warning);border-style: none;border-top-left-radius: -4px;">
+                <div class="container my-auto">
+                    <div class="text-center my-auto copyright"><span style="color: rgb(246, 194, 62);">Copyright © BurgerTech 2021</span></div>
+                </div>
+            </footer>
+        </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/chart.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+    <script src="assets/js/Table-With-Search.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
+</html>
