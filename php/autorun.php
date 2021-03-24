cf9973e2259164ac1e58b160b0864e16d9099f16
RUI YANG <rui.yang@city.ac.uk>
2021-03-24 22:25:23 +0000

    Updated autorun (Still developing)

--- php/autorun.php
+++ php/autorun.php
@@ -1,9 +1,14 @@
 <?php
-$time =15; //how long it run again
-$url = "localhost/php/autorun.php"; // let it access. 
-
-include('../php/alert.php');
-
-sleep( $time );//stop for the time that set.
-file_get_contents ( $url ); //run the php.
+    include('alert.php');
+    $run = include 'autorun_switch.php';
+    $time = 10;// updated every 15 sec.
+    $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+ 
+    //  ToDo the task
+    
+    if(!$run) die(); // config from switch.
+    late_payment_alert();
+    deadline_alert();
+    sleep($time);
+    file_get_contents($url);
 ?>
